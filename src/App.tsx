d2bd474e105a616d95d7186737789399dcf148d9
NHQuan <quan.nguyenhong@buymed.asia>
2023-07-03 16:29:00 +0700

    add logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,61 @@
+/* eslint-disable no-constant-condition */
 import { useState } from 'react'
 import './App.css'
+import data from './data.json';
+import { toast } from 'react-toastify';
 
 function App() {
+  const [arr, setArr] = useState<number[]>([]);
   const [value, setValue] = useState('')
+  const [show, setShow] = useState(false);
+  const [error, setError] = useState(2);
+  const [random, setRandom] = useState(Math.floor(Math.random() * data.length));
+
+
+  const handleSubmit = () => {
+    const newRandom = Math.floor(Math.random() * data.length);
+    if (error !== 0) {
+      toast.error('Error')
+      setError(error - 1);
+    }
+
+    if (value === data[random].paragraph_english) {
+      toast.success('Exactly')
+      while (data.length !== arr.length) {
+        console.log('random', newRandom);
+        if (newRandom !== random && !arr.includes(newRandom)) {
+          setArr([ ...arr, newRandom ])
+          setRandom(newRandom)
+          setValue('');
+          setError(2)
+          break;
+        }
+      }
+    }
+  };
 
   return (
     <div className="flex justify-center items-center">
       <div style={{
-        width: '500px',
+        width: '60vw',
       }}>
-        <h1 style={{ fontSize: 40, marginBottom: 0 }}>English 1</h1>
-        <p style={{ marginTop: 10 }}>Keyword: <span style={{ fontWeight: 700 }}>Board</span></p>
+        <h1 style={{ fontSize: 40, marginBottom: 0 }}>{data[random].paragraph_vietnamese}</h1>
+        <p style={{ marginTop: 10 }}>Keyword: <span style={{ fontWeight: 700 }}>{data[random].keyword}</span>
+        {
+          !show && (
+            <button onClick={() => setShow(true)} style={{
+          fontSize: 12,
+          marginLeft: 4,
+          background: '#6ef36e',
+          color: 'white',
+          padding: '5px 10px'
+        }}>hiện nghĩa</button>
+          )
+        }
+          {
+            show && <span style={{ marginLeft: 4, color: 'green', fontSize: 12 }}>({data[random].translate})</span>
+          }
+        </p>
         <form>
           <input 
             style={{
@@ -27,38 +72,46 @@ function App() {
             onChange={(e) => setValue(e.target.value)}
             className="text-[20px]"
             type="text" value={value} />
+            {
+              error === 0 && <p style={{ color: 'red' }}>{data[random].paragraph_english}</p>
+            }
+            
         </form>
         <div style={{ marginTop: 40, fontWeight: '700' }}>
           <p>More Information</p>
-          <div style={{
-            display: 'flex',
-            alignItems: 'center',
-            marginBottom: 10,
-          }}>
-            <div 
-              style={{
-                paddingLeft: '10px',
-                width: '50%',
-                color: '#333',
-                fontWeight: '700',
-                fontSize: '16px',
-                textAlign: 'center',
-              }}
-            >
-                asdfasdf
+          {
+            data[random].phrases.map((el) => (
+              <div style={{
+                display: 'flex',
+                alignItems: 'center',
+                marginBottom: 10,
+              }}>
+                <div 
+                  style={{
+                    paddingLeft: '10px',
+                    width: '50%',
+                    color: '#333',
+                    fontWeight: '700',
+                    fontSize: '16px',
+                    textAlign: 'center',
+                  }}
+                >
+                    {el.title}
+                  </div>
+                <div
+                  style={{
+                    width: '50%',
+                    textAlign: 'center',
+                    fontWeight: '800',
+                  }}
+                >{el.content}</div>
               </div>
-            <div
-              style={{
-                width: '50%',
-                textAlign: 'center',
-                fontWeight: '800',
-              }}
-            >Absolutely</div>
-          </div>
+            ))
+          }
         </div>
       </div>
       <div style={{paddingTop: 60}}>
-        <button style={{
+        <button onClick={handleSubmit} style={{
           background: 'green',
           color: 'white',
         }}>Submit</button>
