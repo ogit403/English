af9a5920f74a31cbe105a7b3af6d9a03a7bd7ea9
NHQuan <quan.nguyenhong@buymed.asia>
2023-07-03 22:15:02 +0700

    add logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,7 @@
 /* eslint-disable no-constant-condition */
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './App.css'
 import data from './data.json';
-import { toast } from 'react-toastify';
 
 function App() {
   const [arr, setArr] = useState<number[]>([]);
@@ -10,20 +9,20 @@ function App() {
   const [value, setValue] = useState('')
   const [show, setShow] = useState(false);
   const [error, setError] = useState(2);
+  const [loading, setLoading] = useState(false);
   const [random, setRandom] = useState(Math.floor(Math.random() * data.length));
 
 
   const handleSubmit = () => {
     let newRandom = Math.floor(Math.random() * data.length);
     if (value.toLowerCase() !== data[random].paragraph_english.toLowerCase()) {
-      toast.error('Error')
       if (error !== 0) {
         setError(error - 1);
       } 
     }
 
     if (value.toLowerCase() === data[random].paragraph_english.toLowerCase()) {
-      toast.success('Exactly')
+      setLoading(true);
       console.log('data', data, 'arr', arr);
       if (data.length - 1 === arr.length) {
         setDone(true);
@@ -43,15 +42,49 @@ function App() {
         }
       }
     }
+    setLoading(false);
   };
 
+  
+  const handleSound = (value?: string) => {
+    const msg = new SpeechSynthesisUtterance(value || data[random].paragraph_english)
+    window.speechSynthesis.speak(msg)
+  }
 
   return (
     <div className="flex justify-center items-center">
       <div style={{
         width: '60vw',
+        position: 'relative',
       }}>
+        
         <h1 style={{ fontSize: 40, marginBottom: 0 }}>{ !done ? data[random].paragraph_vietnamese : 'Đã hoàn thành tất cả các câu' }</h1>
+        
+        {
+          done && (
+            <p onClick={() => {
+          setArr([])
+          setRandom(Math.floor(Math.random() * data.length))
+          setValue('');
+          setError(2)
+          setShow(false);
+          setDone(false);
+        }} style={{ marginTop: 10, fontSize: 14, cursor: 'pointer' }}>Thử lại hennn</p>
+          )
+        }
+        
+        {
+          loading && (
+            <div style={{
+              position: 'absolute',
+              left: '50%',
+              top: 'calc(50% - 50px)',
+            }}>
+              <div className="loader-5 center"><span></span></div>
+            </div>
+          )
+        }
+        
         {
           !done && (
             <>
@@ -89,12 +122,18 @@ function App() {
             className="text-[20px]"
             type="text" value={value} />
             {
-              error === 0 && <p style={{ color: 'red' }}>{data[random].paragraph_english}
-              <span style={{ color: 'blue', fontSize: 12, marginLeft: 20, cursor: 'pointer' }} onClick={() => navigator.clipboard.writeText(data[random].paragraph_english)}>
-                Sao chép
-              </span>
-              </p>
+              error === 1 && <p style={{ color: 'red', fontSize: 14 }}>Hông đúng òi, thử lại lần nữa nghenn</p>
             }
+             {
+                  error === 0 && <p style={{ color: 'red', fontSize: 14, display: 'flex', justifyContent: 'center', alignItems: 'center' }}>{data[random].paragraph_english}
+                <img onClick={() => handleSound(data[random].paragraph_english)} style={{ cursor: 'pointer', marginLeft: 5 }} src="/public/sound.png" />
+                <span style={{ color: 'blue', fontSize: 12, marginLeft: 20, cursor: 'pointer', fontWeight: 600 }} onClick={() => navigator.clipboard.writeText(data[random].paragraph_english)}>
+                  Copy
+                </span>
+                </p>
+                }
+
+              
             
         </form>
         <div style={{ marginTop: 40, fontWeight: '700' }}>
@@ -114,9 +153,14 @@ function App() {
                     fontWeight: '700',
                     fontSize: '16px',
                     textAlign: 'center',
+                    display: 'flex',
+                    alignItems: 'center',
+                    justifyContent: 'center',
                   }}
                 >
                     {el.title}
+                    <img onClick={() => handleSound(el.title)} style={{ cursor: 'pointer', marginLeft: 5 }} src="/public/sound.png" />
+                    {/* <span  style={{ fontSize: 12, color: 'blue', fontWeight: 600, marginLeft: 5, cursor: 'pointer' }}>Sound</span> */}
                   </div>
                 <div
                   style={{
@@ -130,7 +174,7 @@ function App() {
           }
         </div>
         <div style={{paddingTop: 60}}>
-        <button onClick={handleSubmit} style={{
+        <button disabled={loading} onClick={handleSubmit} style={{
           background: 'green',
           color: 'white',
         }}>Submit</button>
